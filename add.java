ba978e3f29d44b8300b14ded6a5abd0532102521
mrprofex <vivekkumarpatar2007@gmail.com>
2026-07-15 14:57:27 +0530

    i make two pages one for py and another for java to check how it add in the different language

---
+++ add.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class add {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter Your First numeber:");
+        int a = sc.nextInt();
+        System.out.print("Please enter you 2nd number:");
+        int b = sc.nextInt();
+        int c = a + b ;
+        System.out.println("the addition of a + b is :" + c);
+    }
+
+}
+
+// abc string
+// int float double 
---
+++ add.py
@@ -0,0 +1,4 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter 2nd number:"))
+c = a+b
+print("the addition of a and b is :",c)
